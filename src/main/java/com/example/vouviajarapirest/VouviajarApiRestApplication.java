89ae17be9bd63d98fdc293dbf2198412bd830edb
mikefarias <mikesantosfarias@gmail.com>
2020-06-19 20:56:16 -0300

    commit inicial paa subir projeto no git

---
+++ src/main/java/com/example/vouviajarapirest/VouviajarApiRestApplication.java
@@ -0,0 +1,13 @@
+package com.example.vouviajarapirest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VouviajarApiRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VouviajarApiRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/vouviajarapirest/VouviajarApiRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.vouviajarapirest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VouviajarApiRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
