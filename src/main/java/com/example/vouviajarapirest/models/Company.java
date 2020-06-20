a59eb9be3da15ec44df16491edd7f2d36fe590df
mikefarias <mikesantosfarias@gmail.com>
2020-06-20 17:27:02 -0300

    Ajuste em entidade COMPANY

--- src/main/java/com/example/vouviajarapirest/models/Company.java
+++ src/main/java/com/example/vouviajarapirest/models/Company.java
@@ -20,8 +20,8 @@ public class Company implements Serializable{
 	private static final long serialVersionUID = 1L;
 
 	@Id
-	@Column(name = "ID_USER")
-	private Long id;
+	@Column(name = "ID_COMPANY")
+	private Long idCompany;
 	
 	@Column(name = "NAME")
 	private String name; 
@@ -36,9 +36,12 @@ public class Company implements Serializable{
 	
 	@Column(name = "ADDRESS")
 	private Address address;
-	
+
+	@Column(name = "TRAVEL_AGENCY")
+	private TravelAgency travelAgency;
+
 	@Column(name = "CONTACT_TRAVEL_AGENCY")
-	private ContactTravelAgency contactTravelAgency;
+	private TravelAgencyContact contactTravelAgency;
 	
 	@Column(name = "IS_ACTIVE")
 	private boolean isActive;
@@ -49,102 +52,4 @@ public class Company implements Serializable{
 	@Column(name = "CREATED_ON")
 	private OffsetDateTime modifiedOn;
 
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public String getCnpj() {
-		return cnpj;
-	}
-
-	public void setCnpj(String cnpj) {
-		this.cnpj = cnpj;
-	}
-
-	public byte[] getLogo() {
-		return logo;
-	}
-
-	public void setLogo(byte[] logo) {
-		this.logo = logo;
-	}
-
-	public Address getAddress() {
-		return address;
-	}
-
-	public void setAddress(Address address) {
-		this.address = address;
-	}
-
-	public ContactTravelAgency getContactTravelAgency() {
-		return contactTravelAgency;
-	}
-
-	public void setContactTravelAgency(ContactTravelAgency contactTravelAgency) {
-		this.contactTravelAgency = contactTravelAgency;
-	}
-
-	public boolean isActive() {
-		return isActive;
-	}
-
-	public void setActive(boolean isActive) {
-		this.isActive = isActive;
-	}
-
-	public OffsetDateTime getCreatedOn() {
-		return createdOn;
-	}
-
-	public void setCreatedOn(OffsetDateTime createdOn) {
-		this.createdOn = createdOn;
-	}
-
-	public OffsetDateTime getModifiedOn() {
-		return modifiedOn;
-	}
-
-	public void setModifiedOn(OffsetDateTime modifiedOn) {
-		this.modifiedOn = modifiedOn;
-	}
-
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + Arrays.hashCode(logo);
-		result = prime * result
-				+ Objects.hash(address, cnpj, contactTravelAgency, createdOn, id, isActive, modifiedOn, name);
-		return result;
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Company other = (Company) obj;
-		return Objects.equals(address, other.address) && Objects.equals(cnpj, other.cnpj)
-				&& Objects.equals(contactTravelAgency, other.contactTravelAgency)
-				&& Objects.equals(createdOn, other.createdOn) && Objects.equals(id, other.id)
-				&& isActive == other.isActive && Arrays.equals(logo, other.logo)
-				&& Objects.equals(modifiedOn, other.modifiedOn) && Objects.equals(name, other.name);
-	}
-	
 }
